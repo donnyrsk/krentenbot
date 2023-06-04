8277192b68f0978ee690ba7f53609f8401eff0b2
Donny <donnyspek@gmail.com>
2023-06-04 23:01:48 +0200

    Opstart discord bot

---
+++ bot.php
@@ -0,0 +1,38 @@
+<?php
+use Discord\Discord;
+use Discord\WebSockets\Event;
+use Discord\WebSockets\Intents;
+require_once('./vendor/autoload.php');
+require_once('./key.php');
+$key = getKey();
+
+$discord = new Discord(['token'=>$key]);
+$discord->on('ready', function(Discord $discord) {
+    echo 'Bot staat aan!';
+    $discord->on('message', function($message, $discord){
+        $content = $message->content;
+        if(strpos($content, '!') === false) return;
+
+        //Jemoeder responder
+        if($content === '!jemoeder') {
+            $jemoeder = "JOUW MOEDER";
+            $message->reply($jemoeder);
+        }
+
+        //Moppen responder
+        $moppen = [
+            "Twee tieten in een envelop!",
+            "Ik geef zo een mop voor je hoofd",
+            "Sorry, geen zin in.",
+            "Val je moeder lekker lastig ofzo",
+            "Ga buiten spelen alsjeblieft",
+        ];
+
+        if($content === '!mop') {
+            $randomMopArrayNumber = array_rand($moppen);
+            $randomMop = $moppen[$randomMopArrayNumber];
+            $message->reply($randomMop);
+        }
+    });
+});
+$discord->run();
